ffd83836c914f4cf3dfb2296ec1f8fd7dd201c7d
wnsvy4231 <wnsvy4231.wnsvy4231@naver.com>
2020-09-28 10:39:19 +0900

    backup

--- src/main/java/com/test/approval/ApprovalController.java
+++ src/main/java/com/test/approval/ApprovalController.java
@@ -1,8 +1,25 @@
 package com.test.approval;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ApprovalController {
+	
+	@Autowired
+	private ApprovalDAO dao;
+	
+	@RequestMapping(value = "/approvallist.action", method = { RequestMethod.GET })
+	private String approvallist(HttpServletRequest request, HttpServletResponse response) {
 
+		
+
+	
+		return "approval.approvallist";
+	}
 }
